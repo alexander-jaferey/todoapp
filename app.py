15f46faf095662a0d88af8b3ed909dda5dcd32aa
alexander.jaferey <alexj97@gmail.com>
2023-02-17 18:12:45 -0500

    added create to-do item functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.engine.url import URL
 
@@ -23,13 +23,22 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+    description = request.form.get('description', '')
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
 @app.route('/')
 def index():
     return render_template('index.html', data=Todo.query.all()
     )
 
 if __name__ == '__main__':
-    app.debug = True
     with app.app_context():
+        app.debug = True
         db.create_all()
         app.run(host="0.0.0.0", port=5000)
