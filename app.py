66d2faa646c4ed7a54635c9103b4b5a1aed42527
alexander.jaferey <alexj97@gmail.com>
2023-02-17 19:33:05 -0500

    implemented database rollback in case of error

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.engine.url import URL
@@ -25,14 +26,24 @@ class Todo(db.Model):
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify({
-        'description': todo.description
-    })
-
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description2=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
 
 @app.route('/')
 def index():
