fa79b6050a5d98abc77ed96dbaebe6ae8e46f16b
alexander.jaferey <alexj97@gmail.com>
2023-02-17 19:16:13 -0500

    implemented async requests with fetch

--- app.py
+++ app.py
@@ -25,11 +25,13 @@ class Todo(db.Model):
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.form.get('description', '')
+    description = request.get_json()['description']
     todo = Todo(description=description)
     db.session.add(todo)
     db.session.commit()
-    return redirect(url_for('index'))
+    return jsonify({
+        'description': todo.description
+    })
 
 
 @app.route('/')
