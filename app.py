b2bcc575f893a63aa02408deea4e535eab66922d
alexander.jaferey <alexj97@gmail.com>
2023-02-23 15:52:46 -0500

    added list completion and deletion

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class Todo(db.Model):
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
     list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'),  nullable=False)
-    list = db.relationship('TodoList', back_populates='todos')
+    list = db.relationship('TodoList', back_populates='todos', cascade="all, delete")
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
@@ -33,7 +33,8 @@ class TodoList(db.Model):
     __tablename__ = 'todolists'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False)
-    todos = db.relationship('Todo', back_populates='list', lazy=True)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    todos = db.relationship('Todo', back_populates='list', lazy=True, cascade="all, delete")
 
 @app.route('/lists/create', methods=['POST'])
 def create_list():
@@ -80,6 +81,19 @@ def create_todo():
     else:
         return jsonify(body)
     
+@app.route('/lists/<list_id>/set-completed', methods=['POST'])
+def set_completed_list(list_id):
+    try:
+        completed = request.get_json()['completed']
+        db.session.execute(db.update(TodoList).where(TodoList.id == list_id).values(completed=completed))
+        db.session.execute(db.update(Todo).where(Todo.list_id == list_id).values(completed=completed))
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+    
 @app.route('/todos/<todo_id>/set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
     try:
@@ -106,10 +120,22 @@ def delete_todo(todo_id):
         redirect(url_for('index'))
     return jsonify({ 'success': True })
 
+@app.route('/lists/<list_id>', methods=['DELETE'])
+def delete_list(list_id):
+    try:
+        db.session.execute(db.delete(TodoList).where(TodoList.id == list_id))
+        db.session.commit()
+    except:
+        db.session.rollback
+    finally:
+        db.session.close()
+        redirect(url_for('index'))
+    return jsonify({ 'success': True })
+
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template('index.html', 
-    lists=TodoList.query.all(), 
+    lists=TodoList.query.order_by('id').all(), 
     active_list=TodoList.query.get(list_id), 
     todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
     )
